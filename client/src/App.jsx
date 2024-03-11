5d205178f82b060ea38da2317077c1e4ba33d384
KNC2007 <cnkalbach@gmail.com>
2024-03-11 18:13:01 -0400

    debugging render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
