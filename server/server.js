e93fa5f69ab60897e9c3a45cdd6b97357e81ed4b
KNC2007 <cnkalbach@gmail.com>
2024-03-11 18:06:02 -0400

    debugging render errors

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ async function startServer() {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
